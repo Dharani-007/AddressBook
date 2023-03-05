6e1c0f6639ef8d8be9e832a6ba222e25650cb93e
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-06 00:24:18 +0530

    Added : Welcome

--- src/CreateAContact.java
+++ src/CreateAContact.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class CreateAContact {
+public class CreateAContact {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Address Book Program");
+    }
 }
